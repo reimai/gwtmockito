87c6daf2ac8d7ab3a980adeae4582f005a008efd
rei mai <naugadina@gmail.com>
2013-10-14 13:21:37 +0400

    I really should use IDE for every change.

--- gwtmockito/src/main/java/com/google/gwtmockito/GwtMockito.java
+++ gwtmockito/src/main/java/com/google/gwtmockito/GwtMockito.java
@@ -223,8 +223,7 @@ public class GwtMockito {
       // Then check for the exact type provider.
       if (registeredProviders.containsKey(type)) {
           // Its safe - it's the same type provider
-          @SuppressWarnings({"rawtypes", "cast"})
-          return (T) entry.getValue().getFake(type);
+          return (T) registeredProviders.get(type).getFake(type);
       }
 
       // Next see if we have a provider for this type or a supertype.
